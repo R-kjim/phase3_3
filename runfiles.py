771eae24b97748aa88b8182ec0558f596a355a2f
R-kjim <rkayjim@gmail.com>
2024-09-14 17:17:40 +0300

    sql queries accross classes

--- runfiles.py
+++ runfiles.py
@@ -8,6 +8,18 @@ def run():
     Band.create_table()
     Venue.drop_table()
     Venue.create_table()
+    Concert.drop_table()
+    Concert.create_table()
     
-
+    band1=Band.create("kimana","Thika")
+    band2=Band.create("Karainga","Kangema")
+    venue1=Venue.create("City Hall","Nairobi")
+    venue2=Venue.create("Aboretum","Thika")
+    concert1=Concert.create("2020",band1,venue2)
+    concert2=Concert.create("2021",band2,venue1)
+    concert3=Concert.create("2021",band2,venue1)
+    concert4=Concert.create("2020",band2,venue2)
+    band1.play_in_venue("Aboretum","2025") #
+    print(venue2.most_frequent_band())    
+  
 run()
